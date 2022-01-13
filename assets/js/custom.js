6c57b19bdadfdd2f0e596a130276643b049da2b0
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2022-01-13 12:51:26 -0300

    owl carusel

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+  $('.owl-carousel').owlCarousel({
+    stagePadding: 400,
+    loop: true,
+    margin: 40,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    responsive:{
+        0:{
+            items: 1
+        },
+        600:{
+            items: 1
+        },
+        1000:{
+            items: 1
+        }
+      }
+    })
+});
