db05d05a92bb5c885afe9a6f0340e62b64ffa494
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2022-02-10 18:06:51 -0300

    acomodando modal

--- trabajos.php
+++ trabajos.php
@@ -51,7 +51,7 @@
                     </a>
                     <!-- Modal -->
                     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                        <div class="modal-dialog">
+                        <div class="modal-dialog modal-dialog-centered modal-lg">
                         <div class="modal-content">
                             <div class="modal-body">
                                 <iframe width="100%" height="500px" src="https://www.youtube.com/embed/JWRlTezTF2k?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
