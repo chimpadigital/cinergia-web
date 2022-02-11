40f44df53e22cda0050ce9a7acd8f5b876023c23
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2022-02-11 16:12:40 -0300

    ajuste footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,11 @@
 <footer>
     <div class="container">
         <div class="row">
-            <div class="col-md-2 text-center text-md-start mb-4 mb-md-0">
+            <div class="col-xxl-2 text-center text-xxl-start mb-4 mb-xxl-0">
                 <img src="assets/img/logo-cinergia-footer.svg" alt="">
             </div>
-            <div class="col-md-8 d-flex justify-content-center align-self-center">
-                <ul class="d-md-flex text-center mb-4 mb-md-0">
+            <div class="col-xxl-8 d-flex justify-content-center align-self-center">
+                <ul class="d-md-flex text-center mb-4 mb-xxl-0">
                     <li class="d-flex justify-content-center mb-3 mb-md-0">
                         <img src="assets/img/icon-phone.svg" class="me-1" width="20px"><p class="MontserratMedium m-0">+54 9 351 4517095</p>
                     </li>
@@ -19,7 +19,7 @@
                     </li>
                 </ul>
             </div>
-            <div class="col-md-2 d-flex justify-content-center justify-content-md-end align-items-center">
+            <div class="col-xxl-2 d-flex justify-content-center justify-content-xxl-end align-items-center">
                 <a href="https://www.instagram.com/cinergia.estudio.creativo/" target="blank"><img src="assets/img/icon-instagram.svg" width="26px" class="me-3"></a>
                 <a href="https://www.facebook.com/Cinergia.Estudio.Creativo" target="blank"><img src="assets/img/icon-facebook.svg" width="26px" class="me-3"></a>
                 <a href="https://www.youtube.com/channel/UC9pZVM9b6frb2bdq6YRP-aw" target="blank"><img src="assets/img/icon-youtube.svg" width="26px" class="me-3"></a>
