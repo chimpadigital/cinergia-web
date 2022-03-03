8562a72ca226da1354800583607c59e21ee97dbf
Pununuk <pununuk@MacBook-Pro-de-Pununuk.local>
2022-03-03 11:50:35 -0300

    arrow down

--- index.php
+++ index.php
@@ -33,112 +33,22 @@
                 </div>
                 <div class="row vh-80 d-flex align-items-center text-center">
                     <div class="col-12" id="container">
-                        <h3 id="text1" class="fc-pastel mb-0 z-1 wow bounceInUp"></h3>
-                        <h3 id="text2" class="fc-pastel Valky z-1 wow bounceInUp"></h3>
-                        
-                        <svg id="filters">
-                            <defs>
-                                <filter id="threshold">
-                                    <feColorMatrix in="SourceGraphic" type="matrix" values="1 0 0 0 0
-                                                            0 1 0 0 0
-                                                            0 0 1 0 0
-                                                            0 0 0 255 -140" />
-                                </filter>
-                            </defs>
-                        </svg>
-                        <script>
-                            const elts = {
-                            text1: document.getElementById("text1"),
-                            text2: document.getElementById("text2")
-                            };
-
-                            const texts = [
-                            "Buenas Historias",
-                            "Mejores Marcas"
-                            ];
-
-                            const morphTime = 1;
-                            const cooldownTime = 1;
-
-                            let textIndex = texts.length - 1;
-                            let time = new Date();
-                            let morph = 0;
-                            let cooldown = cooldownTime;
-
-                            elts.text1.textContent = texts[textIndex % texts.length];
-                            elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-
-                            function doMorph() {
-                            morph -= cooldown;
-                            cooldown = 0;
-
-                            let fraction = morph / morphTime;
-
-                            if (fraction > 1) {
-                                cooldown = cooldownTime;
-                                fraction = 1;
-                            }
-
-                            setMorph(fraction);
-                            }
-
-                            function setMorph(fraction) {
-                            elts.text2.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-                            elts.text2.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-                            fraction = 1 - fraction;
-                            elts.text1.style.filter = `blur(${Math.min(8 / fraction - 8, 100)}px)`;
-                            elts.text1.style.opacity = `${Math.pow(fraction, 0.4) * 100}%`;
-
-                            elts.text1.textContent = texts[textIndex % texts.length];
-                            elts.text2.textContent = texts[(textIndex + 1) % texts.length];
-                            }
-
-                            function doCooldown() {
-                            morph = 0;
-
-                            elts.text2.style.filter = "";
-                            elts.text2.style.opacity = "100%";
-
-                            elts.text1.style.filter = "";
-                            elts.text1.style.opacity = "0%";
-                            }
-
-                            function animate() {
-                            requestAnimationFrame(animate);
-
-                            let newTime = new Date();
-                            let shouldIncrementIndex = cooldown > 0;
-                            let dt = (newTime - time) / 1000;
-                            time = newTime;
-
-                            cooldown -= dt;
-
-                            if (cooldown <= 0) {
-                                if (shouldIncrementIndex) {
-                                    textIndex++;
-                                }
-
-                                doMorph();
-                            } else {
-                                doCooldown();
-                            }
-                            }
-
-                            animate();
-                        </script>
-                        <!-- <div class="animation-header">
-                            <img src="assets/img/img-animation-header.png"/>
-                        </div> -->
+                        <h3 class="fc-pastel mb-0 z-1 wow bounceInUp">Buenas Historias</h3>
+                        <h3 class="fc-pastel Valky z-1 wow bounceInUp">Mejores Marcas</h3>
+                        <div class="bounce mt-5">
+                            <a href="#down">
+                                <img src="assets/img/arrow-down-home.png" class="wow bounceInUp">
+                            </a>
+                        </div>
                     </div>
                 </div>
             </div>
         </header>
         
-        <div class="container-fluid bg-negro">
+        <div id="down" class="container-fluid bg-negro">
             <div class="row">
                 <div class="col-12 d-flex align-items-center justify-content-center p-0 wow bounceInUp">
-                    <video class="video-home" width="100%" height="400" loop autoplay muted style="object-fit: cover;">
+                    <video class="video-home" width="100%" height="" loop autoplay muted style="object-fit: cover;">
                         <source src="assets/img/bg-spaceship.mp4" type="video/mp4">
                     </video>
                     <a class="lottie-player" data-bs-toggle="modal" data-bs-target="#exampleModal">
